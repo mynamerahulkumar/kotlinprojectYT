e5ae7de070b9388ba7153e47fb023fd0a3fd7245
Rahul Kumar <mynamerahulkumar13@gmail.com>
2025-08-10 13:17:53 +0530

    updated kotlin

--- app/src/main/java/com/tutorial/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/myfirstapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyfirstAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android AI",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyfirstAppTheme {
-        Greeting("Android")
+        Greeting("Android AI")
     }
 }
